6b51966c8c7e4977b7e77ad2249437f3994ea20b
Maxine Tillman <maxltillman@gmail.com>
2021-02-10 10:46:35 +0000

    CSS using styled-components

---
+++ Coding/personalprojects/projects-2021/lorem-ipsum-generator/lorum-ipsum-generator/src/App.styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Header = styled.h1`
+  font-size: 48px;
+  padding: 0px;
+`;
+
+export const Main = styled.div`
+  background-color: white;
+  background-repeat: no-repeat;
+  color: black;
+`;
+
+export const Logo = styled.img`
+  padding: 50px 0px 0px 0px;
+  width: 280px;
+`;
+
+export const Text = styled.p`
+  font-size: 24px;
+  color: #9b8259;
+  line-height: normal;
+  margin: 0px;
+  padding: 0px 0px 20px 0px;
+`;
